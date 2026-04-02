f1cde1e57e01c6e0f30611b7f31eb5345d42c34f
blusalice3 <100254791+blusalice3@users.noreply.github.com>
2026-04-02 14:12:27 +0900

    Delete supabase directory

--- supabase/migrations/008_enable_realtime.sql
+++
@@ -1,4 +0,0 @@
--- Supabase Realtimeでリアルタイム同期するテーブルを登録
-ALTER PUBLICATION supabase_realtime ADD TABLE room_items;
-ALTER PUBLICATION supabase_realtime ADD TABLE room_members;
-ALTER PUBLICATION supabase_realtime ADD TABLE notifications;
