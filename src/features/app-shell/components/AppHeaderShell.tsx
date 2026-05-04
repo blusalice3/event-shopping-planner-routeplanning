47dc520c027d931db79ac819cf55abd31c02465c
blusalice3 <100254791+blusalice3@users.noreply.github.com>
2026-05-05 00:27:11 +0900

    Add files via upload

--- src/features/app-shell/components/AppHeaderShell.tsx
+++ src/features/app-shell/components/AppHeaderShell.tsx
@@ -260,7 +260,11 @@ const AppHeaderShell: React.FC<AppHeaderShellProps> = (props) => {
   return (
     <>
       {(showHeaderBar || showTabBar) && (
-        <header className="bg-white dark:bg-slate-800 shadow-sm sticky top-0 z-10">
+        <header
+          className={`bg-white dark:bg-slate-800 shadow-sm sticky top-0 ${
+            uiSettingsPanelOpen ? 'z-[80]' : 'z-10'
+          }`}
+        >
           {showHeaderBar && (
             <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
               <div>
